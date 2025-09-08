77bfd7251d2e04ac64728fa7c4c8888d87e169f4
siyasjr <siyasjr@gmail.com>
2025-09-08 11:35:58 +0530

    ui-changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Solana Devnet Faucet",
   description: "Features: Airdrop, View Balance, Sign Message, Send Tokens",
+   icons: {
+    icon: "/favicon.png",
+   }
 };
 
 export default function RootLayout({
