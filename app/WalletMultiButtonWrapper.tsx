b0c6d1f1c3b1e39fbb91cb4edb988a94777cc88c
siyasjr <siyasjr@gmail.com>
2025-09-08 10:15:16 +0530

    Fixed hydration issue in next app using a wrapper for WalletMultiButton

---
+++ app/WalletMultiButtonWrapper.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+
+import dynamic from "next/dynamic";
+
+const WalletMultiButton = dynamic (
+    async () => (
+        await import ("@solana/wallet-adapter-react-ui")).WalletMultiButton, {ssr: false}
+    );
+
+    export default WalletMultiButton
